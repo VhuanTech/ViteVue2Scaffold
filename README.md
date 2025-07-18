3e58b7c1a25ac28e37a04a7793b18df506d433d0
Sparks-Lu <lostinfog@126.com>
2025-07-18 13:20:50 +0800

    Initial workable project.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ViteVue2Scaffold
+Scaffold project to use vite and vue 2.
+
+Environment:
+    node 18
+    npm install -g pnpm
+
+Usage:
+    pnpm install
+    pnpm run dev
+    pnpm run build
---
+++ src/views/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>嘎嘎</h1>
+    <el-button @click="showMsg" type="primary">按钮</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text: "嘎嘎嘎嘎"
+      }
+    },
+    methods: {
+      showMsg() {
+        this.$message({
+          type: "success",
+          message: this.text
+        })
+      }
+    }
+  }
+</script>
